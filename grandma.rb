6d1137ae8fed1d447ae412f1c7eb33a6c9fcd496
Steven Ott <stevenkott@icloud.com>
2018-11-03 21:45:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,6 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if phrase = phrase.
+end # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
